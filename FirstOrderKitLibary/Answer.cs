90e95b9bad0e0a749bd21371ef7ef223c3ed75f3
Shani <Shani@DESKTOP-9RLUTQ5>
2025-09-26 12:50:10 +0300

    Add project files.

---
+++ FirstOrderKitLibary/Answer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FirstOrderKitLibary
+{
+    public class Answer
+    {
+           string answerid;
+           string trueFalse;
+           string questionId;
+           string answerText;
+
+        public string Answerid
+        { 
+            get { return this. answerid; }
+         set { this.answerid= value; } 
+        }
+
+        public string TrueFalse
+        {
+            get { return this.trueFalse; }
+            set { this.trueFalse = value; }
+        }
+        public string QuestionId
+        {
+            get { return this.questionId; }
+            set { this.questionId = value; }
+        }
+        [Required(ErrorMessage = "answer Text cannot be empty")]
+        [StringLength(200,MinimumLength =5,ErrorMessage = "answerText")]
+        [FirstLetterCapital(ErrorMessage= "The first letter must be capitaliezed")]
+        public string AnswerText
+        {
+            get { return this.answerText; }
+            set { this.answerText = value; }
+        }
+       
+    }
+}
---
+++ FirstOrderKitLibary/OrderFirstKitViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FirstOrderKitLibary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FirstOrderKitModel
+{
+    public class OrderFirstKitViewModel
+    {
+        public List<Answer> Answers { get; set; }
+        public List<Test> Tests { get; set; }
+        public List<Unit> Units { get; set; }
+
+    }
+}
