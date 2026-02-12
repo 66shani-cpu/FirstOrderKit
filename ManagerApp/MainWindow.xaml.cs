6d5039f1a0bcaf5f7769cf7e59c44d6e882b5d8b
Shani <Shani@DESKTOP-9RLUTQ5>
2026-02-12 23:34:22 +0200

    12.2

--- ManagerApp/MainWindow.xaml.cs
+++ ManagerApp/MainWindow.xaml.cs
@@ -19,17 +19,24 @@ namespace ManagerApp
     {
         StartPage startPage;
         LogInPage logInPage;
+        Update update;
+        Student student;
+        Home home;
+        Reports reports;
+        QuestionManagement questionManagement;
+        Unit_SubjectManagement unit_SubjectManagement;
         public MainWindow()
         {
             InitializeComponent();
-            ViewStartPage();
+            //ViewStartPage();
+            frameMain.Navigate(new StartPage());
         }
         private void ViewStartPage()
         {
-            if(this.startPage == null)
+            if (this.startPage == null)
             {
-                this.startPage= new StartPage();
-                this.frameMain.Content= this.startPage;
+                this.startPage = new StartPage();
+                this.frameMain.Content = this.startPage;
             }
         }
         private void ViewLogInPage()
@@ -40,4 +47,68 @@ namespace ManagerApp
                 this.frameMain.Content = this.logInPage;
             }
         }
+        private void ViewUpdatePage()
+        {
+            if (this.update == null)
+            {
+                this.update = new Update();
+                this.frameMain.Content = this.update;
+            }
+        }
+        private void ViewStudentManagement()
+        {
+            if (this.student == null)
+            {
+                this.student = new Student();
+                this.frameMain.Content = this.student;
+            }
+        }
+        private void ViewHome()
+        {
+            if (this.home == null)
+            {
+                this.home = new Home();
+                this.frameMain.Content = this.home;
+            }
+        }
+        private void ViewReports()
+        {
+            if (this.reports == null)
+            {
+                this.reports = new Reports();
+                this.frameMain.Content = this.reports;
+            }
+        }
+        private void ViewQuestionManagement()
+        {
+            if (this.questionManagement == null)
+            {
+                this.questionManagement = new QuestionManagement();
+                this.frameMain.Content = this.questionManagement;
+            }
+        }
+        private void ViewUnit_SubjectManagement()
+        {
+            if (this.unit_SubjectManagement == null)
+            {
+                this.unit_SubjectManagement = new Unit_SubjectManagement();
+                this.frameMain.Content = this.unit_SubjectManagement;
+            }
+        }
+
+        private void hyperlinkLogin_Click(object sender, RoutedEventArgs e)
+        {
+            ViewLogInPage();
+        }
+        private void hyperlinkStartPage_Click(object sender, RoutedEventArgs e)
+        {
+            //ViewStartPage();
+            frameMain.Navigate(new StartPage());
+        }
+        private void hyperlinkLogin_Click(object sender, RoutedEventArgs e)
+        {
+            ViewLogInPage();
+        }
+
     }
+}
